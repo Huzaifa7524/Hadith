51d6fe5f36ef183a6c81fb0415e3a7d340d96066
Huzaifa7524 <huzaifatahir7524@gmail.com>
2023-10-12 18:50:42 +0500

    aa

--- imageandtext/imageandtext/settings.py
+++ imageandtext/imageandtext/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# Define the directory where additional static files are located.
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Replace with your directory path
+
+# Define the directory where `collectstatic` will store the collected static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
